253b8f43279c1fd380c0b1ad36265684a349b73d
unknown <caio.dev17@gmail.com>
2024-06-20 12:43:28 -0300

    :sparkles: feat: PageSection component has been created

---
+++ components/PageSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+
+section {
+  background-color: $background-color;
+}
+</style>
